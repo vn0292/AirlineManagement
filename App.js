1e7a8b6d2c2c628b3d03a26f563cfe0bfe788349
vn0292 <nagulapallivyshnavi@gmail.com>
2024-06-11 14:55:07 -0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,20 @@
+import react,{Component} from 'react';
+import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import 'animate.css'
+import NavBar from './Components/NavBar'
+
+
+import React from 'react';
+
+
+
+function App() {
+
+
+  return (
+   <div><NavBar/></div>
+  );
+}
+
+export default App;
